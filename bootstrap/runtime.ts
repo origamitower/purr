642b3151bb87d1df263b7121573d674f56bf1af2
Quildreen Motta <quildreen@gmail.com>
2018-06-10 21:17:16 -0300

    Working runtime

--- bootstrap/runtime.ts
+++ bootstrap/runtime.ts
@@ -1,35 +1,80 @@
-export function $equals(left: any, right: any): boolean {
-  if (left === null) {
-    return right === null;
-  } else if (typeof left === "object") {
-    return $objectEquals(left, right);
-  } else {
-    return left === right;
-  }
+function $equals(a: any, b: any) {
+  return a === b;
 }
 
-export function $notEquals(left: any, right: any): boolean {
-  if (left === null) {
-    return right !== null;
-  } else if (typeof left === "object") {
-    return !$objectEquals(left, right);
-  } else {
-    return left !== right;
-  }
+function $not_equals(a: any, b: any) {
+  return a !== b;
 }
 
-export function $plus(left: any, right: any): any {
-  return left + right;
+function $gt(a: any, b: any) {
+  return a > b;
 }
 
-export function $minus(left: any, right: any): any {
-  return left - right;
+function $lt(a: any, b: any) {
+  return a < b;
 }
 
-export function $mul(left: any, right: any): any {
-  return left * right;
+function $gte(a: any, b: any) {
+  return a >= b;
 }
 
-export function $div(left: any, right: any): any {
-  return left / right;
+function $lte(a: any, b: any) {
+  return a <= b;
 }
+
+function $plus(a: any, b: any) {
+  return a + b;
+}
+
+function $minus(a: any, b: any) {
+  return a - b;
+}
+
+function $div(a: any, b: any) {
+  return a / b;
+}
+
+function $mul(a: any, b: any) {
+  return a * b;
+}
+
+function $or(a: any, b: any) {
+  return a || b;
+}
+
+function $and(a: any, b: any) {
+  return a && b;
+}
+
+function $not(a: any) {
+  return !a;
+}
+
+function $fail(message) {
+  return new Error(message);
+}
+
+function $get(a: any, p: any) {
+  return a[p];
+}
+
+function $at(a: any, p: any) {
+  return a[p];
+}
+
+function $vector(...items: any[]) {
+  return items;
+}
+
+function $map(...pairs: any[]) {
+  return new Map(pairs);
+}
+
+function $int(a: any) {
+  return Number(a);
+}
+
+function $dec(a: any) {
+  return Number(a);
+}
+// <runtime ends here>
