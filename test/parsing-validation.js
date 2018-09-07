70587d2df4befd3a07bac8b1a299c7ebe5f6f7b1
Quildreen Motta <quildreen@gmail.com>
2018-09-07 10:23:05 -0300

    wip

---
+++ test/parsing-validation.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const { parse } = require("../boot/parser");
+
+describe("Parsing (validation only)", () => {
+  const root = path.join(__dirname, "fixtures/parsing");
+  const files = fs.readdirSync(root).map(name => ({
+    name: name,
+    contents: fs.readFileSync(path.join(root, name), "utf8")
+  }));
+
+  for (const file of files) {
+    it(file.name, () => {
+      parse(file.contents);
+    });
+  }
+});
