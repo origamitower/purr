1f905a87526b21e5f5c8538f258186b8f8c78436
Quildreen Motta <quildreen@gmail.com>
2018-09-07 16:41:23 -0300

    wip

--- boot/origami.js
+++ boot/origami.js
@@ -1,15 +1,40 @@
+#!/usr/bin/env node
 const { parse } = require("./parser");
 const { generate } = require("./codegen");
 const { inspect } = require("util");
 const fs = require("fs");
+const path = require("path");
 
-const [file, compile] = process.argv.slice(2);
-const source = fs.readFileSync(file, "utf8");
-console.log("-- AST");
-const ast = parse(source);
-console.log(inspect(ast, false, null, true));
-if (compile) {
-  console.log("-- JS");
-  const js = generate(ast).code;
-  console.log(js);
+function read(f) {
+  return fs.readFileSync(f, "utf8");
 }
+
+function compile(program) {
+  const js = generate(parse(program)).code;
+  return `${runtime}\n${js}`;
+}
+
+const runtime = read(path.join(__dirname, "runtime.js"));
+
+// CLI
+require("yargs")
+  .command("ast <file>", "shows the ast for <file>", {}, argv => {
+    const program = read(argv.file);
+    console.log(inspect(parse(program), false, null, true));
+  })
+  .command("compile <file>", "compiles <file> to JavaScript", {}, argv => {
+    const program = read(argv.file);
+    console.log(compile(program));
+  })
+  .command("run <file>", "runs the main() declaration in <file>", {}, argv => {
+    const params = argv.params || [];
+    require.extensions[".origami"] = (mod, file) => {
+      const program = read(file);
+      mod._compile(compile(program), file);
+    };
+    const mod = require(argv.file);
+    mod.main(params);
+  })
+  .help()
+  .strict()
+  .demandCommand(1).argv;
