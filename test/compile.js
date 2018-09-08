da6b1fc0a0f13a67398d2fa1b64f5525c2e41c51
Quildreen Motta <quildreen@gmail.com>
2018-09-08 14:31:17 -0300

    Compilation tests

---
+++ test/compile.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const { register } = require("../bootstrap");
+
+register();
+
+describe("Compiling", () => {
+  const root = path.join(__dirname, "fixtures/compile");
+  const files = fs
+    .readdirSync(root)
+    .filter(x => /\.origami$/.test(x))
+    .map(name => ({
+      name: name,
+      path: path.join(root, name)
+    }));
+
+  for (const file of files) {
+    describe(file.name, () => {
+      const mod = require(file.path);
+      for (const [name, fn] of Object.entries(mod)) {
+        if (name.startsWith("test_")) {
+          it(name.replace(/^test_/, "").replace(/_/g, " "), () => fn());
+        }
+      }
+    });
+  }
+});
